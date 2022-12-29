3eddb82fc6461be5a42bda75d4996b8afa2d720f
Matheus Monteiro Alves <matheus.alves@intelie.com.br>
2022-12-28 22:11:04 -0300

    Alterando o Path do endPoint

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,13 +1,22 @@
 package com.mercadolivro.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+//path padrão
+@RequestMapping("customer")
 class CustomerController {
 
     @GetMapping
     fun helloWorld(): String {
-        return "Hello World"
+        return "customer 1"
+    }
+
+    //sub path
+    @GetMapping("/2")
+    fun customer2(): String {
+        return "customer 2"
     }
 }
